67749f285211bd01f48b5acecad43af343d35d2a
RawadAltagiuri <rawadaltagury@gmail.com>
2023-05-27 21:43:06 +0300

    NewCode
    Added the multiadd function

--- GreedyAlgorithm.cpp
+++ GreedyAlgorithm.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <unordered_set>
+#include "NODECLASS.H"
 using namespace std;
-
-class Node{
-    public:
-        int color;
-        vector<Node*>adj;
-        Node(){
-            this->color=0;
-        }
-};
 
 void solution(Node& graph, int& colors){
     if(graph.color!=0) return; //if the graph has been colored return, stopping condition.
@@ -37,17 +29,16 @@ void solution(Node& graph, int& colors){
 
 int main(){
     Node* graph = new Node[6]; //forming the graph;
-    graph[0].adj = { &graph[1], &graph[2] ,&graph[5]};
-    graph[1].adj = { &graph[0], &graph[2], &graph[3] };
-    graph[2].adj = { &graph[0], &graph[1], &graph[3], &graph[5]};
-    graph[3].adj = { &graph[1], &graph[2], &graph[4] };
-    graph[4].adj = { &graph[3], &graph[5] };
-    graph[5].adj = { &graph[0], &graph[2], &graph[4]};
-
-
+    //the function multiadd takes two parameters, the first one is the vertex you want to add the adjacents to, and the second is a vector of the indexes of the parameters.
+    graph->multiadd(0, {1, 2, 5});                          // graph[0].adj = { &graph[1], &graph[2] ,&graph[5]};
+    graph->multiadd(1, {0, 2, 3});                          // graph[1].adj = { &graph[0], &graph[2], &graph[3] };
+    graph->multiadd(2, {0, 1, 3, 5});                       // graph[2].adj = { &graph[0], &graph[1], &graph[3], &graph[5]};
+    graph->multiadd(3, {1, 2, 4});                      // graph[3].adj = { &graph[1], &graph[2], &graph[4] };
+    graph->multiadd(4, {3, 5});                        // graph[4].adj = { &graph[3], &graph[5] };
+    graph->multiadd(5, {0, 2, 4});                  // graph[5].adj = { &graph[0], &graph[2], &graph[4]};
     int colors = 0;
     solution(graph[0], colors);
-    cout << "Colors assigned to nodes:W" << endl;
+    cout << "Colors assigned to nodes:" << endl;
     for (int i = 0; i < 6; i++) {
         cout << "Node " << i << ": " << graph[i].color << endl;
     }
