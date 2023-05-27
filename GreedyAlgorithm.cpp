306c0095ac828ab890254134ad7f52da15f6f673
RawadAltagiuri <rawadaltagury@gmail.com>
2023-05-27 17:50:28 +0300

    comments

--- GreedyAlgorithm.cpp
+++ GreedyAlgorithm.cpp
@@ -13,27 +13,30 @@ class Node{
 };
 
 void solution(Node& graph, int& colors){
-    if(graph.color!=0) return;
+    if(graph.color!=0) return; //if the graph has been colored return, stopping condition.
     unordered_set<int>set;
     for(Node* i : graph.adj){
         if(i->color!=0){
             set.insert(i->color);
         }
     }
-    int color = 1;
+    int color = 1; //we start with the minimum color which is one
     for(auto s : set){
-        if(set.count(color))
+        if(set.count(color)) //if the adjacent nodes have the color then increase to a different color.
             color++;
     }
-    if(color>colors) colors=color;
-    graph.color = color;
+
+    if(color>colors) colors=color; //this line is to check how many colors we used.
+
+    graph.color = color; //set the graph color
+
     for(Node* i : graph.adj){
-        solution(*i, colors);
+        solution(*i, colors); //visit its adjacent graphs.
     }
 }
 
 int main(){
-    Node* graph = new Node[6];
+    Node* graph = new Node[6]; //forming the graph;
     graph[0].adj = { &graph[1], &graph[2] ,&graph[5]};
     graph[1].adj = { &graph[0], &graph[2], &graph[3] };
     graph[2].adj = { &graph[0], &graph[1], &graph[3], &graph[5]};
